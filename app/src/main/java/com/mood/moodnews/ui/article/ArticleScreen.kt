f52fe9a4f81e46c605d6265cde11f8541e0bc0da
mood <moodfactor@hotmail.com>
2022-07-23 16:42:13 +0200

    implemented Article screen with Bottom and top bar

--- app/src/main/java/com/mood/moodnews/ui/article/ArticleScreen.kt
+++ app/src/main/java/com/mood/moodnews/ui/article/ArticleScreen.kt
@@ -1,21 +1,108 @@
 package com.mood.moodnews.ui.article
 
+import android.content.Context
+import android.content.Intent
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mood.moodnews.R
+import com.mood.moodnews.data.Result
+import com.mood.moodnews.data.posts.impl.BlockingFakePostsRepository
+import com.mood.moodnews.data.posts.post3
 import com.mood.moodnews.model.Post
+import com.mood.moodnews.ui.theme.MoodNewsTheme
+import com.mood.moodnews.ui.utils.BookmarkButton
+import com.mood.moodnews.ui.utils.FavoriteButton
+import com.mood.moodnews.ui.utils.ShareButton
+import com.mood.moodnews.ui.utils.TextSettingsButton
 import com.mood.moodnews.utils.isScrolled
+import kotlinx.coroutines.runBlocking
+
+/**
+ * Stateless Article Screen that displays a single post adapting the UI to different
+ * screen sizes
+ *
+ * @param post (state)  item to display
+ * @param isExpandedScreen (state) if the navigation icon should be shown
+ * @param onBack (event) request navigation back
+ * @param isFavorite (state) is this item currently a favorite
+ * @param onToggleFavorite (event) request this post to toggle it's favorite state
+ * @param lazyListState (state) the [lazyListState] for the article content
+ */
+
+@Composable
+fun ArticleScreen(
+    post: Post,
+    isExpandedScreen: Boolean,
+    onBack: () -> Unit,
+    isFavorite: Boolean,
+    onToggleFavorite: () -> Unit,
+    modifier: Modifier = Modifier,
+    lazyListState: LazyListState = rememberLazyListState()
+) {
+    var showUnimplementedActionDialog by rememberSaveable { mutableStateOf(false) }
+    if (showUnimplementedActionDialog) {
+        FunctionalityNotAvailablePopup { showUnimplementedActionDialog = false }
+    }
+
+    Row(modifier.fillMaxSize()) {
+        val context = LocalContext.current
+        ArticleScreenContent(
+            post = post,
+            // Allow opening the Drawer if the screen is not expanded
+            navigationIconContent = if (!isExpandedScreen) {
+                {
+                    IconButton(onClick = onBack) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.cd_navigate_up),
+                            tint = MaterialTheme.colors.primary
+                        )
+                    }
+                }
+            } else {
+                null
+            },
+            // Show the bottom bar if the screen is not expanded
+            bottomBarContent = if (!isExpandedScreen) {
+                {
+                    BottomBar(
+                        onUnimplementedAction = { showUnimplementedActionDialog = true },
+                        isFavorite = isFavorite,
+                        onToggleFavorite = onToggleFavorite,
+                        onSharePost = { sharePost(post, context) },
+                    )
+                }
+            } else {
+                { }
+            },
+            lazyListState = lazyListState
+        )
+    }
+}
+
+
+
 
 /**
  * Stateless Article Screen that displays a single post.
@@ -51,7 +138,7 @@ private fun ArticleScreenContent(
                         Text(
                             text = stringResource(
                                 id = R.string.published_in,
-                                arrayOf((post.publication?.name) ?: "")
+                                post.publication?.name ?: ""
                             ),
                             style = MaterialTheme.typography.subtitle2,
                             color = LocalContentColor.current,
@@ -78,3 +165,112 @@ private fun ArticleScreenContent(
     }
 }
 
+/**
+ * Bottom bar for Article screen
+ *
+ * @param onUnimplementationAction (event) called when the user performs an unimplemented action
+ * @param isFavorite (state) if this post is currently  a favorite
+ * @param onToggleFavorite (event) request this post toggle it's favorite status
+ * @param onSharePost (event) request this post to be shared
+ */
+
+@Composable
+private fun BottomBar(
+    onUnimplementedAction: () -> Unit,
+    isFavorite: Boolean,
+    onToggleFavorite: () -> Unit,
+    onSharePost: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Surface(elevation = 8.dp, modifier = modifier) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically, modifier = Modifier
+                .windowInsetsPadding(WindowInsets.navigationBars.only(WindowInsetsSides.Vertical))
+                .height(56.dp)
+                .fillMaxWidth()
+        ) {
+            FavoriteButton(onClick = onUnimplementedAction)
+            BookmarkButton(isBookmarked = isFavorite, onClick = onToggleFavorite)
+            ShareButton(onClick = onSharePost)
+            Spacer(modifier = Modifier.weight(1f))
+            TextSettingsButton(onClick = onUnimplementedAction)
+
+        }
+
+    }
+}
+
+/**
+ * Display a popup explaining functionality not available.
+ *
+ * @param onDismiss (event) request the popup be dismissed
+ */
+
+@Composable
+private fun FunctionalityNotAvailablePopup(onDismiss: () -> Unit) {
+    AlertDialog(onDismissRequest = onDismiss,
+        text = {
+            Text(
+                text = stringResource(id = R.string.article_functionality_not_available),
+                style = MaterialTheme.typography.body2
+            )
+        },
+        confirmButton = {
+            TextButton(onClick = onDismiss) {
+                Text(text = stringResource(id = R.string.close))
+
+            }
+        }
+    )
+}
+
+/**
+ * Show a share sheet for a post
+ *
+ * @param post to share
+ * @param context Android context to show th share sheet in
+ */
+fun sharePost(post: Post, context: Context) {
+    val intent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_TITLE, post.title)
+        putExtra(Intent.EXTRA_TEXT, post.url)
+
+    }
+    context.startActivity(
+        Intent.createChooser(
+            intent,
+            context.getString(R.string.article_share_post)
+        )
+    )
+}
+
+@Preview("Article screen")
+@Preview("Article screen (dark)", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview("Article screen (big font)", fontScale = 1.5f)
+@Composable
+fun PreviewArticleDrawer() {
+    MoodNewsTheme {
+        val post = runBlocking {
+            (BlockingFakePostsRepository().getPost(post3.id) as com.mood.moodnews.data.Result.Success).data
+        }
+        ArticleScreen(post, false, {}, false, {})
+    }
+}
+
+@Preview("Article screen navrail", device = Devices.PIXEL_C)
+@Preview(
+    "Article screen navrail (dark)",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    device = Devices.PIXEL_C
+)
+@Preview("Article screen navrail (big font)", fontScale = 1.5f, device = Devices.PIXEL_C)
+@Composable
+fun PreviewArticleNavRail() {
+    MoodNewsTheme {
+        val post = runBlocking {
+            (BlockingFakePostsRepository().getPost(post3.id) as Result.Success).data
+        }
+        ArticleScreen(post, true, {}, false, {})
+    }
+}
